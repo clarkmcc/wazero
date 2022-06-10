7d93ef81eef5eb28cdbf1e30a9f34a8b8f92b2a1
Takeshi Yoneda <takeshi@tetrate.io>
2022-06-10 10:29:33 +0900

    pass all spectests........
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/integration_test/spectest/v2/spec_test.go
+++ internal/integration_test/spectest/v2/spec_test.go
@@ -36,15 +36,10 @@ func TestCompiler(t *testing.T) {
 			"simd_f64x2_pmin_pmax.json", "simd_f32x4_pmin_pmax.json", "simd_int_to_int_extend.json",
 			"simd_i64x2_extmul_i32x4.json", "simd_i32x4_extmul_i16x8.json", "simd_i16x8_extmul_i8x16.json",
 			"simd_i16x8_q15mulr_sat_s.json", "simd_i16x8_extadd_pairwise_i8x16.json", "simd_i32x4_extadd_pairwise_i16x8.json",
-			"simd_i32x4_dot_i16x8.json", "simd_conversions.json", "simd_i32x4_trunc_sat_f32x4.json":
+			"simd_i32x4_dot_i16x8.json", "simd_conversions.json", "simd_i32x4_trunc_sat_f32x4.json",
+			"simd_splat.json", "simd_load.json", "simd_i32x4_trunc_sat_f64x2.json":
 			// TODO: implement on arm64.
 			return runtime.GOARCH == "amd64"
-		case "simd_i32x4_trunc_sat_f64x2.json":
-			return false
-		case "simd_load.json":
-			return false
-		case "simd_splat.json":
-			return false
 		default:
 			return true
 		}
